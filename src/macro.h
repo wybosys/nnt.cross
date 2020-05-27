a33b921665bde6678fb747be6af7a632b5f6f634
wybosys <wybosys@gmail.com>
2020-05-27 11:57:09 +0800

    prj

--- src/macro.h
+++ src/macro.h
@@ -5,54 +5,73 @@
 #define NNT_NS nnt
 #endif
 
-#define NNT_BEGIN namespace NNT_NS {
+#define NNT_BEGIN    \
+    namespace NNT_NS \
+    {
 #define NNT_END }
 
 #define USE_NNT using namespace NNT_NS;
-#define USE_NNT_NS(ns) USE_NNT; using namespace NNT_NS::ns;
+#define USE_NNT_NS(ns) \
+    USE_NNT;           \
+    using namespace NNT_NS::ns;
 
-#define NNT_BEGIN_NS(ns) NNT_BEGIN namespace ns {
-#define NNT_END_NS() } NNT_END
+#define NNT_BEGIN_NS(ns)   \
+    NNT_BEGIN namespace ns \
+    {
+#define NNT_END_NS() \
+    }                \
+    NNT_END
 
 #define NNT_FRIEND(cls) friend class cls;
 #define NNT_NOCOPY(cls) \
-private:                 \
+private:                \
     cls(cls &) = delete;
 #define NNT_PRIVATECLASS(cls) cls##Private
 
 #define NNT_CLASS_PREPARE(cls) \
-    class cls;                  \
+    class cls;                 \
     class NNT_PRIVATECLASS(cls);
 
-#define NNT_CLASS_DECL(cls)                                         \
-protected:                                                           \
-    typedef NNT_PRIVATECLASS(cls) private_class_type;               \
-    friend class NNT_PRIVATECLASS(cls);                             \
-    private_class_type *d_ptr = nullptr;                             \
+#define NNT_CLASS_DECL(cls)                                               \
+protected:                                                                \
+    typedef NNT_PRIVATECLASS(cls) private_class_type;                     \
+    friend class NNT_PRIVATECLASS(cls);                                   \
+    private_class_type *d_ptr = nullptr;                                  \
     friend private_class_type *nnt::DPtr<cls, private_class_type>(cls *); \
-                                                                     \
-private:                                                             \
+                                                                          \
+private:                                                                  \
     NNT_NOCOPY(cls);
 
 #define NNT_CLASS_CONSTRUCT(...) \
     d_ptr = new private_class_type(__VA_ARGS__);
 #define NNT_CLASS_DESTORY() \
-    delete d_ptr;            \
+    delete d_ptr;           \
     d_ptr = nullptr;
 
 #define NNT_SINGLETON_DECL(cls) \
-public:                          \
-    static cls &shared();
-#define NNT_SINGLETON_IMPL(cls, ...)       \
-    static cls *_shared = nullptr;          \
-    cls &cls::shared()                      \
-    {                                       \
-        if (_shared == nullptr)             \
-        {                                   \
-            _shared = new cls(__VA_ARGS__); \
-        }                                   \
-        return *_shared;                    \
-    }
+public:                         \
+    static cls &shared();       \
+    static void free_shared();  \
+    static bool is_shared();
+#define NNT_SINGLETON_IMPL(cls, ...)                \
+    static cls *_##cls##_shared = nullptr;          \
+    cls &cls::shared()                              \
+    {                                               \
+        if (!_##cls##_shared)                       \
+        {                                           \
+            _##cls##_shared = new cls(__VA_ARGS__); \
+        }                                           \
+        return *_##cls##_shared;                    \
+    }                                               \
+    void cls::free_shared()                         \
+    {                                               \
+        if (_##cls##_shared)                        \
+        {                                           \
+            delete _##cls##_shared;                 \
+            _##cls##_shared = nullptr;              \
+        }                                           \
+    }                                               \
+    bool cls::is_shared() { return nullptr != _##cls##_shared; }
 
 #define __NNT_RAW(L) L
 #define __NNT_COMBINE(L, R) L##R
@@ -112,7 +131,7 @@ typedef string system_string;
 #endif
 
 template <class T, class TP = typename T::private_class_type>
-static TP* DPtr(T* obj)
+static TP *DPtr(T *obj)
 {
     return obj->d_ptr;
 }
