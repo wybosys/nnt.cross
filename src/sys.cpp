bfeb154eb795484a56bcc6a2a3734f576f8644de
wybosys <wybosys@gmail.com>
2020-06-13 20:58:32 +0800

    getpid

--- src/sys.cpp
+++ src/sys.cpp
@@ -1,15 +1,37 @@
 #include "cross.hpp"
 #include "sys.hpp"
-#include <thread>
-#include <sstream>
+
+#ifdef NNT_WINDOWS
+
+#include <Windows.h>
+
+#endif
+
+#ifdef NNT_UNIXLIKE
+
+#include <sys/types.h>
+#include <unistd.h>
+
+#endif
 
 CROSS_BEGIN
 
+#ifdef NNT_WINDOWS
+
 pid_t get_thread_id()
 {
-    ::std::ostringstream oss;
-    oss << ::std::this_thread::get_id();
-    return std::stoull(oss.str());
+    return ::GetCurrentThreadId();
 }
+
+#endif
+
+#ifdef NNT_UNIXLIKE
+
+pid_t get_thread_id()
+{
+    return ::getpid();
+}
+
+#endif
 
 CROSS_END
