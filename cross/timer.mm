c11906aa993eca129e79a214b1f39d7edddefaca
wybosys <wybosys@gmail.com>
2020-07-07 20:21:00 +0800

    mac定时器

--- cross/timer.mm
+++ cross/timer.mm
@@ -1,7 +1,123 @@
 #import "cross.hpp"
 #import "timer.hpp"
+#include "logger.hpp"
+#include "threads.hpp"
+#include <map>
+#include <atomic>
 
 CROSS_BEGIN
 
+class NSTimerThread
+: public Thread
+{
+public:
+    
+    NSTimerThread()
+    : Thread("n2c.timer")
+    {
+        repeat = FOREVER;
+    }
+    
+    NNT_SINGLETON_DECL(NSTimerThread);
+  
+    ::std::atomic<Timer::timer_t> idr;
+    ::std::map<Timer::timer_t, NSTimer*> timers;
+    NSRunLoop *loop = nullptr;
+    ::std::mutex mtx;
+    
+    void run(NSTimer *tmr)
+    {
+        NNT_AUTOGUARD(_mtx_loop);
+        if (loop) {
+            [loop addTimer:tmr forMode:NSDefaultRunLoopMode];
+        } else {
+            _delay_timers.emplace_back(tmr);
+        }
+    }
+    
+    virtual void main() override
+    {
+        if (!loop)
+        {
+            NNT_AUTOGUARD(_mtx_loop);
+            loop = [NSRunLoop currentRunLoop];
+            
+            for (auto &e:_delay_timers) {
+                [loop addTimer:e forMode:NSDefaultRunLoopMode];
+            }
+            _delay_timers.clear();
+        }
+        
+        [loop runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+    }
+    
+private:
+    
+    ::std::mutex _mtx_loop;
+    ::std::vector<NSTimer*> _delay_timers;
+    
+};
+
+NNT_SINGLETON_IMPL(NSTimerThread);
+
+void NSTimerThread::_shared_init()
+{
+    start();
+}
+
+static Timer::timer_t SetTimer(double time, Timer::tick_t&& tick, bool repeat)
+{
+    auto &thd = NSTimerThread::shared();
+    
+    auto tmrid = thd.idr++;
+    auto cb = ::std::forward<Timer::tick_t>(tick); // 复制一份，oc的block不会像cxx的lambda抛出编译错误
+    auto tmr = [NSTimer timerWithTimeInterval:time repeats:repeat block:^(NSTimer * _Nonnull timer) {
+        if (!repeat)
+        {
+            NNT_AUTOGUARD(thd.mtx);
+            thd.timers.erase(tmrid);
+        }
+        
+        cb();
+    }];
+    
+    NNT_AUTOGUARD(thd.mtx);
+    thd.timers[tmrid] = tmr;
+    thd.run(tmr);
+    
+    return tmrid;
+}
+
+Timer::timer_t Timer::SetTimeout(double time, tick_t&& tick)
+{
+    return SetTimer(time, ::std::forward<tick_t>(tick), false);
+}
+
+void Timer::CancelTimeout(timer_t tmrid)
+{
+    auto &thd = NSTimerThread::shared();
+
+    NNT_AUTOGUARD(thd.mtx);
+    
+    auto fnd = thd.timers.find(tmrid);
+    if (fnd == thd.timers.end()) {
+        Logger::Warn("没有找到定时器，定时器已经结束或者传入的定时器id错误");
+        return;
+    }
+    
+    auto tmr = fnd->second;
+    [tmr invalidate];
+    thd.timers.erase(fnd);
+}
+
+Timer::timer_t Timer::SetInterval(double time, tick_t&& tick)
+{
+    return SetTimer(time, ::std::forward<tick_t>(tick), true);
+}
+
+void Timer::CancelInterval(timer_t tmrid)
+{
+    CancelTimeout(tmrid);
+}
 
 CROSS_END
--- mac/test-macos/test-macos/test.mm
+++ mac/test-macos/test-macos/test.mm
@@ -1,8 +1,10 @@
 #import "test.h"
 
 #import <cross/cross.hpp>
+#import <cross/logger.hpp>
 #import <cross/str.hpp>
 #import <cross/threads.hpp>
+#import <cross/timer.hpp>
 
 USE_CROSS;
 
@@ -24,8 +26,43 @@ void test_thread()
     wait.wait();
 }
 
+void test_timer()
+{
+    auto tmrs = new CoTimers(0.001);
+    tmrs->add(0.2, 5, [=]()
+              {
+        static size_t count = 0;
+        Logger::Info("定时器回调");
+        if (++count == 5)
+        {
+            delete tmrs;
+        }
+    });
+    tmrs->start();
+    
+    Timer::SetTimeout(1, []()
+                      {
+        Logger::Info("1s定时器");
+    });
+    
+    auto tmr = make_shared<Timer::timer_t>();
+    *tmr = Timer::SetInterval(2, [=]()
+                              {
+        Logger::Info("2s循环定时器");
+        static size_t count = 5;
+        if (--count == 0)
+        {
+            Logger::Info("取消2s循环定时器");
+            Timer::CancelInterval(*tmr);
+        }
+    });
+}
+
 void Test()
 {
     test_sys();
     test_thread();
+    test_timer();
+    
+    MainThread::shared().exec();
 }
